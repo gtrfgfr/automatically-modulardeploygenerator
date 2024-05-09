ca3794dce209e2918478b473e7b898ba25266b0f
gtrfgfr <isjcndirawilson@hotmail.com>
2024-05-09 12:54:37 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-function uniquePaths(m, n) {
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1;
-  }
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 1;
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+function canPartitionKSubsets(nums, k) {
+  const sum = nums.reduce((acc, val) => acc + val, 0);
+  if (sum % k !== 0) return false;
+  const target = sum / k;
+  nums.sort((a, b) => b - a);
+  if (nums[0] > target) return false;
+  const visited = new Array(nums.length).fill(false);
+  return backtrack(0, 0, k);
+  function backtrack(start, currentSum, groups) {
+    if (groups === 1) return true;
+    if (currentSum === target) return backtrack(0, 0, groups - 1);
+    for (let i = start; i < nums.length; i++) {
+      if (!visited[i] && currentSum + nums[i] <= target) {
+        visited[i] = true;
+        if (backtrack(i + 1, currentSum + nums[i], groups)) return true;
+        visited[i] = false;
+      }
     }
+    return false;
   }
-  return dp[m - 1][n - 1];
 }

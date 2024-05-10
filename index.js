11369569fda0764ac4a23a801503db096f57fdac
gtrfgfr <isjcndirawilson@hotmail.com>
2024-05-11 01:34:13 +0700

    start

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-function canPartitionKSubsets(nums, k) {
-  const sum = nums.reduce((acc, val) => acc + val, 0);
-  if (sum % k !== 0) return false;
-  const target = sum / k;
-  nums.sort((a, b) => b - a);
-  if (nums[0] > target) return false;
-  const visited = new Array(nums.length).fill(false);
-  return backtrack(0, 0, k);
-  function backtrack(start, currentSum, groups) {
-    if (groups === 1) return true;
-    if (currentSum === target) return backtrack(0, 0, groups - 1);
-    for (let i = start; i < nums.length; i++) {
-      if (!visited[i] && currentSum + nums[i] <= target) {
-        visited[i] = true;
-        if (backtrack(i + 1, currentSum + nums[i], groups)) return true;
-        visited[i] = false;
-      }
-    }
-    return false;
-  }
-}
+// Alice should have the initial balance minus tranfer amount
+if (aliceBalanceAfter[0].amount !== ALICE_INITIAL_BALANCE - TRANSFER_AMOUNT)
+  throw new Error("Alice's balance after transfer is incorrect");
+
+// Sponsor should have the initial balance minus gas
+if (sponsorBalanceAfter[0].amount >= SPONSOR_INITIAL_BALANCE)
+  throw new Error("Sponsor's balance after transfer is incorrect");
